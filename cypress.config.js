2ed3f90f4d72fe4e6eaade5484e660f890dfc251
Victor Charao <vcharao98@gmail.com>
2026-07-14 16:56:54 +0200

    Added test reporting using cypress-mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,21 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
   e2e: {
     baseUrl: 'https://front.serverest.dev',
     specPattern: 'cypress/{api,e2e}/**/*.cy.js',
     env: {
       apiUrl: 'https://serverest.dev',
     },
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
+    },
   },
 })
